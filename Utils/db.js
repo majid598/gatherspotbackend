216f1e4413f5f7b12c2a2eddf7210bb7167a3bd5
Mr raju <143610202+majid598@users.noreply.github.com>
2024-04-23 14:55:01 +0630

    Update db.js

--- Utils/db.js
+++ Utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = (uri) => {
   mongoose
-    .connect(uri, { dbName: "Instagram-Clone" })
+    .connect(uri, { dbName: "gatherSpot" })
     .then((data) => console.log(`DB Connected To: ${data.connection.host}`))
     .catch((err) => {
       throw err;
